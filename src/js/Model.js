f659578ae0c193c3c4e0ddced4de66ade5234716
AdiM32 <adididiadidas@gmail.com>
2018-12-02 09:27:42 +0200

    start bulidinf model

--- src/js/Model.js
+++ src/js/Model.js
@@ -1,6 +1,9 @@
 let model = [];
+let vars = [];
 const buildStruct = (...keys) => ((...values) => keys.reduce((obj, key, i) => {obj[key] = values[i]; return obj;} , {}));
 const Row = buildStruct('Line', 'Type', 'Name', 'Condition', 'Value');
+const Var = buildStruct('name', 'value', 'isLocal');
+
 
 const type_func = {'Program': (pc) => parseBody(pc.body),
     'FunctionDeclaration': (pc) => parseFunctionDeclaration(pc.loc.start.line, pc.params, pc.id.name, pc.body),
@@ -21,6 +24,22 @@ const sideType_func = {'Identifier': (s) => {return s.name;},
     'UnaryExpression': (s) =>  {return s.operator + pareOneSide(s.argument);},
     'UpdateExpression': (s) => {return s.argument.name + s.operator;}};
 
+const type_func_sub = {'VariableDeclaration': (pc) => subVariableDeclaration(pc.declarations),
+    'IfStatement': (pc) => subIfStatement(pc.test, pc.consequent, pc.alternate)};
+
+const sideType_func_sub = {'Identifier': (s) => subIdentifier(s),
+    'Literal': (s) => {return s.raw;},
+    'BinaryExpression': (s) => {return '(' + subBinaryExpression(s) + ')';},
+    'MemberExpression': (s) => {return s.object.name + '[' + subOneSide(s.property) + ']';},
+    'UnaryExpression': (s) =>  {return s.operator + subOneSide(s.argument);},
+    'UpdateExpression': (s) => {return s.argument.name + s.operator;}};
+
+function subIdentifier(s) {
+    let res = s.name;
+    vars.forEach((v) => {if (v.name === s.name && v.isLocal){res = v.value;}});
+    return res;
+}
+
 function buildModel(parsedCode) {
     if (parsedCode.type in type_func){
         type_func[parsedCode.type](parsedCode);
@@ -32,13 +51,19 @@ function parseBody(body) {
 }
 
 function parseFunctionDeclaration(line, params, name, body) {
-    model.push(Row(line, 'function declaration', name, '', ''));
-    parseParam(params);
-    buildModel(body);
+    let paramsNames = parseParam(params);
+    symbolicSubstitution(body.body, paramsNames);
+}
+
+function symbolicSubstitution(body, prams) {
+    body.forEach((b) => {if (b.type in type_func_sub){type_func_sub[b.type](b);}});
+    return body + prams;
 }
 
 function parseParam(params) {
-    params.forEach((param) => model.push(Row(param.loc.start.line, 'variable declaration', param.name, '', '')));
+    let paramsNames = [];
+    params.forEach((param) => {paramsNames.push(param.name); vars.push(Var(param.name, null, false));});
+    return paramsNames;
 }
 
 function parseVariableDeclaration(declarations) {
@@ -48,14 +73,30 @@ function parseVariableDeclaration(declarations) {
     });
 }
 
+function subVariableDeclaration(declarations) {
+    declarations.forEach((element) => {
+        vars.push(Var(element.id.name, element.init === null? null : subOneSide(element.init), true));
+    });
+}
+
 function pareOneSide(side) {
     return sideType_func[side.type](side);
+}
+
+function subOneSide(side) {
+    return sideType_func_sub[side.type](side);
 }
 
 function parseBinaryExpression(binaryExpression) {
     return pareOneSide(binaryExpression.left) + ' ' +
         binaryExpression.operator + ' ' +
         pareOneSide(binaryExpression.right);
+}
+
+function subBinaryExpression(binaryExpression) {
+    return subOneSide(binaryExpression.left) + ' ' +
+        binaryExpression.operator + ' ' +
+        subOneSide(binaryExpression.right);
 }
 
 function parseWhileStatement(test, body, line) {
@@ -69,6 +110,17 @@ function parseAssignmentExpression(left, right, line) {
 
 function parsedIfStatement(line, type, test, consequent, alternate) {
     model.push(Row(line, type[0] === 'E'? 'else if statement': 'if statement', '', parseBinaryExpression(test), ''));
+    buildModel(consequent);
+    if (alternate !== null) {
+        if (alternate.type === 'IfStatement') {
+            alternate.type = 'ElseIfStatement';
+        }
+        buildModel(alternate);
+    }
+}
+
+function subIfStatement(test, consequent, alternate) {
+    let sub_test = subBinaryExpression(test);
     buildModel(consequent);
     if (alternate !== null) {
         if (alternate.type === 'IfStatement') {
